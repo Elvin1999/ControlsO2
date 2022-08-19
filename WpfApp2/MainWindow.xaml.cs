9783da4fe5370c1e9beb1d9fdbae1741672c9795
e.camalzade <e.camalzade@STHQ0127-10>
2022-08-19 11:34:14 +0400

    Add project files.

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //private void Button_Click(object sender, RoutedEventArgs e)
+        //{
+        //    this.Close();
+        //}
+        //public int Count { get; set; } = 0;
+        //private void btn_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Count++;
+        //    btn.Content = Count.ToString();
+        //}
+
+        //private void repeat_button_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Count++;
+        //    repeat_button.Content = Count.ToString();
+        //}
+
+        //private void toogle_button_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Count++;
+        //    toogle_button.Content = Count.ToString();
+        //}
+    }
+}
